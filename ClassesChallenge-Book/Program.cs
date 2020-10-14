28fd3f73c0f1bc115ed4f167edf0bf318fd91b2b
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-10-14 18:53:04 -0400

    Add project files.

---
+++ ClassesChallenge-Book/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ClassesChallenge_Book
+{
+    public class Book
+    {
+        public string bookName;
+        public string autorName;
+        public double bookPrice;
+
+        public void logBook()
+        {
+            Console.WriteLine(bookName +" by " + autorName + " the price is: " + bookPrice);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Book book1 = new Book();
+            book1.bookName = "stormlight Archives: The Way of Kings ";
+            book1.autorName = "Brandon Sanderson ";
+            book1.bookPrice = 9.99;
+
+            book1.logBook();
+        }
+    }
+}
